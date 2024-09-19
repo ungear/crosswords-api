05519bd823d81bbacbb72de7e7c1c2cd1f90cbe8
Mikhail Istomin <redeemer@inbox.ru>
2024-09-19 18:52:53 -0300

    feat(db): add CreatedAt timestamp

--- db/create.mjs
+++ db/create.mjs
@@ -7,7 +7,8 @@ async function initDb() {
       CREATE TABLE Words (
         Id INTEGER PRIMARY KEY AUTOINCREMENT,
         Word TEXT NOT NULL,
-        Answer TEXT NOT NULL
+        Answer TEXT NOT NULL,
+        CreatedAt DATETIME DEFAULT CURRENT_TIMESTAMP
       );`);
   });
 
