2ce049b8c7f04e69c6ded944e6b9ee001246c48d
Mikhail Istomin <redeemer@inbox.ru>
2024-10-28 16:38:15 -0300

    feat: add swagger

--- server.ts
+++ server.ts
@@ -26,35 +26,65 @@ const opts: RouteShorthandOptions = {
   }
 }
 
-server.get('/ping', opts, async (request, reply) => {
-  return { pong: 'it worked!' }
+server.register(require('@fastify/swagger'), {
+  swagger: {
+    info: {
+      title: 'Crosswords API',
+      description: 'Crosswords API',
+      version: 'alpha',
+      consumes: ['application/json'],
+      produces: ['application/json']
+    }
+  }
 })
 
-server.post('/word', async (request: any, reply: any) => {
-  const word = request.body.word;
-  const answer = request.body.answer;
-  addWord(word, answer);
-  return reply.send({ success: true });
+server.register(require('@fastify/swagger-ui'), {
+  routePrefix: '/documentation',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  uiHooks: {
+    onRequest: function (request: any, reply: any, next: any) { next() },
+    preHandler: function (request: any, reply: any, next: any) { next() }
+  },
+  staticCSP: true,
+  transformStaticCSP: (header: any) => header,
+  transformSpecification: (swaggerObject: any , request: any, reply: any) => { return swaggerObject },
+  transformSpecificationClone: true
 })
 
-server.get('/words', async (request: any, reply: any) => {
-  const words = await getWords();
-  return reply.send({ words });
-})
-
-server.post('/words', async (request: any, reply: any) => {
-  const words = request.body.words;
-  const questions = await aiService.getAnswerForWords(words);
-  questions?.forEach((pair) => {
-    addWord(pair[0], pair[1]);
-  });
-  return reply.send({ success: questions && questions.length > 0 });
-})
-
-server.get('/crossword', async (request, reply) => {
-  const words = await getRandomQuestions(10);
-  const crossword = crosswordService.generateCrossword(words);
-  return new CrosswordDto(crossword);
+server.register(async function(server){
+  server.get('/ping', opts, async (request, reply) => {
+    return { pong: 'it worked!' }
+  })
+  
+  server.post('/word', async (request: any, reply: any) => {
+    const word = request.body.word;
+    const answer = request.body.answer;
+    addWord(word, answer);
+    return reply.send({ success: true });
+  })
+  
+  server.get('/words', async (request: any, reply: any) => {
+    const words = await getWords();
+    return reply.send({ words });
+  })
+  
+  server.post('/words', async (request: any, reply: any) => {
+    const words = request.body.words;
+    const questions = await aiService.getAnswerForWords(words);
+    questions?.forEach((pair) => {
+      addWord(pair[0], pair[1]);
+    });
+    return reply.send({ success: questions && questions.length > 0 });
+  })
+  
+  server.get('/crossword', async (request, reply) => {
+    const words = await getRandomQuestions(10);
+    const crossword = crosswordService.generateCrossword(words);
+    return new CrosswordDto(crossword);
+  })
 })
 
 const start = async () => {
